3ddfb046d4f38b9057e9608fa1576a35482ded6b
Kule123e45 <40210831+Kule123e45@users.noreply.github.com>
2021-01-08 15:42:43 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# Client and User Credentials 
+# The Credentials used in this project are sensitive so have not been committed to the public repo
+# If you wish to recreate this project, replace the values attributed to the variables below with your own credentials
+
+#Spotify Credentials
+username = "YOUR_USERNAME"
+SPOTIPY_CLIENT_ID = "YOUR_SPOTIFY_APP_CLIENT_ID"
+SPOTIPY_CLIENT_SECRET = "YOUR_SPOTIFY_APP_CLIENT_SECRET"
+SPOTIPY_REDIRECT_URI = "YOU_REDIRECT_URI" # Note: this uri must be whitelisted on your spotify app dashboard
+
+#Twilio Credentials
+TWILIO_ACCOUNT_SID = 'YOUR_TWILIO_ACCOUNT_SID'
+TWILIO_AUTH_TOKEN = 'YOUR_TWILIO_AUTH_TOKEN'
+
+

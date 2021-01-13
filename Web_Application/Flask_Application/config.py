11e65d719e062379618e0ad9bedd224af2583c17
Kule123e45 <40210831+Kule123e45@users.noreply.github.com>
2021-01-13 14:33:55 +0000

    Update config.py

--- Web_Application/Flask_Application/config.py
+++ Web_Application/Flask_Application/config.py
@@ -1,19 +1,20 @@
 #client and user credentials config file
 
 #Spotify credentials
-username = "Luke Hillery"
-username_str = "kule123e45"
-SPOTIPY_CLIENT_ID = "3f203afa2be240ffbcaa571e12eee03e"
-SPOTIPY_CLIENT_SECRET = "6947e86380b949d1b5665125bf4dfd4c"
-SPOTIPY_REDIRECT_URI = "http://localhost:5000/authorised"
+username = "Your username"
+username_str = "Your username"
+SPOTIPY_CLIENT_ID = "Your client id"
+SPOTIPY_CLIENT_SECRET = "Your client secret"
+SPOTIPY_REDIRECT_URI = "Your redirect uri"
 
 #Twilio Credentials
-TWILIO_ACCOUNT_SID = 'AC557c01d9968537fb4f638d0d03c27af7'
-TWILIO_AUTH_TOKEN = 'd879ff2fdb91e03532f6e45b7f70e4a3'
+TWILIO_ACCOUNT_SID = 'Your Twilio Account SID'
+TWILIO_AUTH_TOKEN = 'Your Twilio Auth Token'
 
 #Personal details
-MY_MOBILE_NUMBER = '+447952165272'
+MY_MOBILE_NUMBER = 'Your mobile number'
+FRIEND_MOBILE_NUMBER = "Friend's mobile number"
 
 #ThingSpeak credentials
-THINGSPEAK_CHANNEL_1 = 'RYJ2D37RI5EGO0MP'
-THINGSPEAK_CHANNEL_2 = 'L4P6XPRO3ODASMS3'
+THINGSPEAK_CHANNEL_1 = 'Thingspeak Channel API Write Key (1)'
+THINGSPEAK_CHANNEL_2 = 'Thingspeak Channel API Write Key (2)'

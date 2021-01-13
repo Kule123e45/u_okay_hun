b8aa360c35cc1f160455dfb5dc153c678739a236
Kule123e45 <40210831+Kule123e45@users.noreply.github.com>
2021-01-13 17:29:45 +0000

    Create config.py

---
+++ Data_Collection/config.py
@@ -0,0 +1,20 @@
+#client and user credentials config file
+
+#Spotify credentials
+username = "Your username"
+username_str = "Your username"
+SPOTIPY_CLIENT_ID = "Your client id"
+SPOTIPY_CLIENT_SECRET = "Your client secret"
+SPOTIPY_REDIRECT_URI = "Your redirect uri"
+
+#Twilio Credentials
+TWILIO_ACCOUNT_SID = 'Your Twilio Account SID'
+TWILIO_AUTH_TOKEN = 'Your Twilio Auth Token'
+
+#Personal details
+MY_MOBILE_NUMBER = 'Your mobile number'
+FRIEND_MOBILE_NUMBER = "Friend's mobile number"
+
+#ThingSpeak credentials
+THINGSPEAK_CHANNEL_1 = 'Thingspeak Channel API Write Key (1)'
+THINGSPEAK_CHANNEL_2 = 'Thingspeak Channel API Write Key (2)'
